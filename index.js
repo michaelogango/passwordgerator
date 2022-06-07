0fefd064b71c98abb8e738d087e8fc8a42ee3727
Michael Odhiambo <michaelodhiambo@Michaels-MacBook-Air.local>
2022-06-07 08:33:18 +0300

    updated javascript and connected js to Html

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
+//defining the three parts, Poutput, Password, and copy
+document.getElementById('poutput').value = genpasword();
+document.form[0].submit()
+    //var button = document.getElementById('Password').addEventListener('click', genpasword);
+    //const button1 = document.querySelector('butn1');
 
 
-var password= document.getElementById("password");
-function genPasswordLower(){
-    return String.fromCharCode(Math.floor(Math.random()*26)+97);
-}
-function genPasswordUpper(){
-    return String.fromCharCode(Math.floor(Math.random()*26)+65);
-}
-function number(){
-    return String.fromCharCode(Math.floor(Math.random()*10)+48);
-} 
-function symbols(){
-    const symbol="!@#$%^&*()_+=~:;<>,.";
-    return symbol[Math.floor(Math.random()*symbol.length)];
-} 
-const randomgen={
-    lower:genPasswordLower,
-    upper:genPasswordUpper,
-    num:number,
-    symbol:symbols
-};
+//function to fandomize password
+let passLength = 12;
+let PasswordChar = "0123456789 abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+let PasswordValue = '';
 
 
-function genpasword(lower,upper,num, symbol){
-    let generatedPassword='';
-    const typesCount=lower+upper+num+symbol;
-    console.log('typecount:' ,typesCount);
-}
+//function to generate the password. 
+const genpasword = () => {
+    PasswordValue = '';
 
-
-
-/* var char="0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passLength=17;
-    var password="";
-
-
-    for( var i=0;i<=passLength;i++){
-        var x=(math.floor(Math.random()*char.length));
-        password+=char.substring(x,x+1);
+    for (let i = 0; i < passLength; i++) {
+        let num = (Math.floor(Math.random() * PasswordChar.length));
+        PasswordValue += PasswordChar.substring(num, num + 1);
     }
+    //console.log(PasswordValue);
+    // password = PasswordValue;
+    return (PasswordValue);
+}
+console.log(genpasword());
+console.log("hello world 2");
 
-    //code to activate the button
-    document.getElementById("password").value=password;
-*/
+//add button listener- constantly listening for a click
+//button = addEventListener('click', genpasword);
+
+
